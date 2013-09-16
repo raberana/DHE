793cef247b2ae4df591a46b11bf86d3afc90f5b0
roj <raberana@live.com>
2013-09-17 01:03:46 +0800

    first commit

---
+++ MainApp/History.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    public  class History
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual DateTime StartDate { get; set; }
+        public virtual DateTime EndDate { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string SeriesCoverPath { get; set; }
+    }
+}
